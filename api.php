906c5fd16203c38a39f58a5b447d2fd96b692615
timojaask <timojaask@gmail.com>
2014-07-14 12:35:43 +0300

    Some thoughts on how this could work
    Created a new table in the database with same structure and messages as before, but with additional parent_id column. Default value is 0, which means it's a first message in a conversation. All the reply messages get their parent_id set to the id of the first message.
    There are some TODOs in the code, also one that's not mentioned: the functions should not return anything if the input params are bad (this is related to Joonas' suggestion).

--- api.php
+++ api.php
@@ -8,131 +8,132 @@ class Api extends CI_Controller {
 		exit;
 	}
 	
+	private function fixed_server_time()
+	{
+		// TODO: Ruben needs to fix the clock of the server, it's off by 14 min and 56 from UTC
+		$server_time_utc_offset = (14 * 60 + 56);
+		$fixed_time = (time() - $server_time_utc_offset);
+		return $fixed_time;
+	}
+	
+	public function server_time()
+	{
+		echo $this->fixed_server_time();
+	}
+	
 	public function add()
 	{
-		// FUCK MVC! I'm sticking with "VC" only
-		$data = array(
-			'latitude'	=>	$this->input->post('lat'),
-			'longitude'	=>	$this->input->post('long'),
-			'text'		=>	$this->input->post('text'),
-			'pubTime'	=>	time() + $this->input->post('pubt')
-		);
-
-		$this->db->insert('whispers', $data);
-		// Return the ID of the newly inserted message
-		echo $this->db->insert_id();
-	}
-
-	public function add_json()
-	{
+		$current_time = $this->fixed_server_time();
 		$json = json_decode(trim(file_get_contents('php://input')));
 		$data = array(
-			'latitude'	=>	$json->latitude,
-			'longitude'	=>	$json->longitude,
-			'text'		=>	$json->text,
-			'pubTime'	=>	time() + $json->pubTime
+			'latitude' => $json->latitude,
+			'longitude' => $json->longitude,
+			'text' => $json->text,
+			'pubTime' => $current_time + $json->pubDelay,
+			'parent_id' => $json->parent_id
 		);
-
-		$this->db->insert('whispers', $data);
+		$this->db->insert('skene_messages', $data);
 		// Return the ID of the newly inserted message
 		echo $this->db->insert_id();
 	}
 	
-	public function get_servertime()
+	public function get()
 	{
-			echo time();
-	}
-	public function get_latest($count = 200, $timestamp = '0')
-	{
-		if ($this->input->get('timestamp')) $timestamp = $this->input->get('timestamp');
-		if ($this->input->get('count')) $count = $this->input->get('count');
-		if ($this->input->get('req_id'))
-		{
-			// Request ID is not required, but if supplied, it is simply echoed back
-			echo "{\"req_id\":\"". $this->input->get('req_id') ."\"}";
+		// Available parameters:
+		// lat: center location latitude
+		// long: center location longitude
+		// radius: radius of area in meters
+		// timestamp (default: 0): get messages posted after this time. Format: Unix timestamp
+		// count (default: 50): maximum number of messages to return
+		// parent_id: (default 0): only get messages with this parent_id
+
+		
+		if ($this->input->get('timestamp')) {
+			$timestamp = $this->input->get('timestamp');
+		} else {
+			$timestamp = 0;
+		}
+		if ($this->input->get('count')) {
+			$count = $this->input->get('count');
+		} else {
+			$count = 50;
+		}
+		if ($this->input->get('parent_id')) {
+			$parent_id = $this->input->get('parent_id');
+		} else {
+			$parent_id = 0;
+			$radius = $this->input->get('radius') / 1000;
+			$lat = $this->input->get('lat');
+			$long = $this->input->get('long');
+
+			// Get min/max latitude and longitue to select messages from the database
+	        	$R = 6371;  // earth's mean radius, km
+	        	// First-cut bounding box (in degrees)
+	        	$max_lat = $lat + rad2deg($radius/$R);
+	        	$min_lat = $lat - rad2deg($radius/$R);
+	        	// Compensate for degrees longitude getting smaller with increasing latitude
+	        	$max_long = $long + rad2deg($radius/$R/cos(deg2rad($lat)));
+	        	$min_long = $long - rad2deg($radius/$R/cos(deg2rad($lat)));
 		}
 		
-		$this->db->start_cache();
-		$query = $this->db->order_by("id", "desc")->get_where('whispers', array('pubTime >' => $timestamp, 'pubTime <' => time()), $count);
+		$current_time = $this->fixed_server_time();
+
+        	// Compose SQL query
+        	if ($parent_id == 0) {
+        		$query = $this->db->order_by("id", "desc")->get_where('skene_messages', array('parent_id = ' => 0, 'latitude >=' => $min_lat, 'latitude <=' => $max_lat, 'longitude >=' => $min_long, 'longitude <=' => $max_long, 'pubTime >=' => $timestamp, 'pubTime <=' => $current_time), $count);
+        	} else {
+        		$query = $this->db->order_by("id", "desc")->get_where('skene_messages', array('parent_id = ' => $parent_id, 'pubTime >=' => $timestamp, 'pubTime <=' => $current_time), $count);
+        	}
+        
+        	// TODO: Filter out messages that are outside of defined radius
+	
+        	// Run database query
 		$result = $query->result();
-		$this->db->stop_cache();
-		$code = '';
-		$i = 0;
-		
-		if ($this->input->get('json')) // mobile
-		{
-			echo json_encode($result);
-		}
-		else
-		{
-			foreach($result as $whisper) {
-		        $code .= '<tr><td>'.$whisper->text.'</td>';
-		        if ($i <= $count){
-			        $code .= '<td id="last_whisper">'.$whisper->pubTime.'</td></tr>';
-			    } else {
-					$code .= '<td>'.$whisper->pubTime.'</td></tr>';    
-				}
-				$i++;
-			}
-	    
-			echo $code;
-	    }
+
+		// Output in JSON format
+		echo json_encode($result);
 	}
 	
-	public function get_local_whispers($count = 50)
+	public function get_map_data()
 	{
-		if ($this->input->get('timestamp')) $timestamp = $this->input->get('timestamp'); else $timestamp = 0;
+		// Get N latest conversations.
+		// TODO: Try to filter not by first message date, but by last reply date - tricky?
+
+		// Available parameters:
+		// min_lat: min/max lat/long of message
+		// max_lat: --
+		// min_long: --
+		// max_long: --
+		// timestamp (default: 0): get messages posted after this time. Format: Unix timestamp
+		// count (default: 50): maximum number of messages to return
+
+		// Get required parameters
 		$min_lat = $this->input->get('min_lat');
 		$max_lat = $this->input->get('max_lat');
 		$min_long = $this->input->get('min_long');
 		$max_long = $this->input->get('max_long');
-		if ($this->input->get('req_id'))
-		{
-			// Request ID is not required, but if supplied, it is simply echoed back
-			echo "{\"req_id\":\"". $this->input->get('req_id') ."\"}";
+
+		// Get optional parameters
+		if ($this->input->get('count')) {
+			$count = $this->input->get('count');
+		} else {
+			$count = 50;
 		}
-		
-		if ($this->input->get('newCode')){
-            		/* 
-            		?newCode=1 is required in URL, implemented so that old code and programs will not break for now, later it can be removed
-            		radius in kilometers, lat, long required parameters for this section
-            		*/
-            		$R = 6371;  // earth's mean radius, km
-            
-            		// first-cut bounding box (in degrees)
-            		$max_lat = $lat + rad2deg($radius/$R);
-            		$min_lat = $lat - rad2deg($radius/$R);
+		if ($this->input->get('timestamp')) {
+			$timestamp = $this->input->get('timestamp');
+		} else {
+			$timestamp = 0;
+		}
 
-            		// compensate for degrees longitude getting smaller with increasing latitude
-            		$max_long = $long + rad2deg($radius/$R/cos(deg2rad($lat)));
-            		$min_long = $long - rad2deg($radius/$R/cos(deg2rad($lat)));
-            		$query = $this->db->order_by("id", "desc")->get_where('whispers', array('latitude >=' => $min_lat, 'latitude <=' => $max_lat, 'longitude >=' => $min_long, 'longitude <=' => $max_long, 'pubTime >=' => $timestamp, 'pubTime <=' => time()), $count);
-        	}
-        	else{
-            		$query = $this->db->order_by("id", "desc")->get_where('whispers', array('latitude >=' => $min_lat, 'latitude <=' => $max_lat, 'longitude >=' => $min_long, 'longitude <=' => $max_long, 'pubTime >=' => $timestamp, 'pubTime <=' => time()), $count);
-        	}
+		$current_time = $this->fixed_server_time();
 
+		// Compose SQL query
+		$query = $this->db->order_by("id", "desc")->select('latitude, longitude, pubTime')->get_where('skene_messages', array('parent_id = ' => 0, 'latitude >=' => $min_lat, 'latitude <=' => $max_lat, 'longitude >=' => $min_long, 'longitude <=' => $max_long, 'pubTime >=' => $timestamp, 'pubTime <=' => $current_time), $count);
+
+        	// Run database query
 		$result = $query->result();
-		$code = '';
-		$i = 0;
-		
-		if ($this->input->get('json'))
-		{
-			echo json_encode($result);
-		}
-		else
-		{
-			foreach($result as $whisper) {
-		        $code .= '<tr><td>'.$whisper->text.'</td>';
-		        if ($i <= $count){
-			        $code .= '<td id="last_whisper">'.$whisper->pubTime.'</td></tr>';
-			    } else {
-					$code .= '<td>'.$whisper->pubTime.'</td></tr>';    
-				}
-				$i++;
-			}
-	    
-			echo $code;
-		}
+
+		// Output in JSON format
+		echo json_encode($result);
 	}
 }
